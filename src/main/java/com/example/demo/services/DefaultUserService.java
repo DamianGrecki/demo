04fc272dff7a6abdc566d0378ca7090deb958fe9
Damian Grecki <d.grecki@onet.pl>
2025-11-16 20:21:29 +0100

    Wydzielenie metod w UserService

--- src/main/java/com/example/demo/services/DefaultUserService.java
+++ src/main/java/com/example/demo/services/DefaultUserService.java
@@ -8,6 +8,7 @@ import com.example.demo.models.requests.UserRegisterRequestBody;
 import com.example.demo.models.responses.UserRegisterResponse;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 class DefaultUserService implements UserService {
@@ -19,14 +20,24 @@ class DefaultUserService implements UserService {
     }
 
     @Override
+    @Transactional
     public UserRegisterResponse register(UserRegisterRequestBody request) {
         String email = request.getEmail();
         String password = request.getPassword();
         String confirmedPassword = request.getConfirmPassword();
 
         comparePasswords(password, confirmedPassword);
-        saveUser(email, password);
+        addUser(email, password);
         return new UserRegisterResponse(true, email);
+    }
+
+    private void addUser(String email, String password) {
+        validateEmailUniqueness(email);
+        User user = new User(
+                email,
+                encodePassword(password)
+        );
+        userRepository.save(user);
     }
 
     private void comparePasswords(String password, String confirmedPassword) {
@@ -35,15 +46,16 @@ class DefaultUserService implements UserService {
         }
     }
 
-    private void saveUser(String email, String password) {
+    private String encodePassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    private void validateEmailUniqueness(String email) {
         if (userRepository.findByEmail(email).isPresent()) {
             throw new ResourceAlreadyExistsException(
-                    String.format("User with email '%s' already exist.", email)
+                    String.format("User with email '%s' already exists.", email)
             );
         }
-        String encodedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
-        User user = new User(email, encodedPassword);
-        userRepository.save(user);
     }
 
 }
